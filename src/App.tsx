e1d5441ece4ecee5684f3c1473b02de25a4f934e
kita <r.kita@remodela.jp>
2023-10-21 15:01:35 +0900

    デバイスとmediaStreamを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useMount } from 'react-use';
+import useDevice from './hooks/useDevice';
+import { getMediaTrackSettings } from './utils';
 
-function getVideoSize(mediaStream: MediaStream) {
-  const track = mediaStream.getVideoTracks()[0]
-  const { width, height } = track.getSettings()
-  console.log({ width, height })
+const videoConstraints: MediaTrackConstraints = {
+  facingMode: 'environment'
 }
 
 function App() {
-  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null)
+  const deviceHook = useDevice();
+  const [settings, setSettings] = useState<MediaTrackSettings>({});
+ 
 
-  useEffect(() => {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' }, audio: false })
-    setMediaStream(stream)
-    getVideoSize(stream)
-  }, [])
+  useMount(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: false })
+    setSettings(getMediaTrackSettings(stream))
+  })
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <pre>
+        {JSON.stringify(deviceHook.videoDevices, null, 2)}
+      </pre>
+      <pre>
+        {JSON.stringify(deviceHook.orientation, null, 2)}
+      </pre>
+      <pre>
+        {JSON.stringify(settings, null, 2)}
+      </pre>
     </>
   )
 }
